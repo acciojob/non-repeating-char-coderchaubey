c332bb43dc3c818d5b86b18e3b5ea1fcc0429843
coderchaubey <115737868+coderchaubey@users.noreply.github.com>
2023-04-18 12:12:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function solve(s){
+    let hashTable={};
+    for(let a of s){
+        if(hashTable[a]) hashTable[a]++;
+        else hashTable[a]=1;
+    }
+    for(let a of s){
+        if(hashTable[a]===1) return a;
+    }
+}
+
+let s=prompt("Enter a String");
+alert(solve(s));
